025b226610d512a95366e25e32c62c321513e9f8
ggxsam <grgsamira10@gmail.com>
2025-07-20 13:49:20 +0545

    feat: add sidebar component

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,10 +1,7 @@
 import { Outlet } from 'react-router';
 
-import Sidebar from '@/components/layouts/sidebar';
-
 export const DashboardLayout = () => (
   <div className='flex h-screen'>
-    <Sidebar />
     <main className='flex-1 overflow-auto'>
       <Outlet />
     </main>
