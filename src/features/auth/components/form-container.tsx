8438867d904f61a852d71f3c108758c82b42ba30
suvadrashrestha <jeenashrestha04@gmail.com>
2025-07-20 14:27:34 +0545

    fix: used kebab case

--- src/features/auth/components/form-container.tsx
+++ src/features/auth/components/form-container.tsx
@@ -1,4 +1,4 @@
-import Cardwithform from './cardwithform';
+import Cardwithform from './card-with-form';
 
 const FormContainer = () => (
   <div className='flex flex-col items-center justify-center gap-[30px] py-5'>
@@ -7,7 +7,7 @@ const FormContainer = () => (
       <p className='text-xl text-gray-700'>Logger System</p>
       <div className='h-[2px] w-[50px] bg-gray-700'></div>
     </div>
-    <div className='relative flex h-[370px] w-full items-center justify-center bg-[var(--green)]'>
+    <div className='bg-green relative flex h-[370px] w-full items-center justify-center'>
       <Cardwithform />
     </div>
   </div>
