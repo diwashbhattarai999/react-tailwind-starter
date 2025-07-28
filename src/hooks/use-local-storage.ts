ce18e5fa1760a367d732a386686c3ce62b93817f
Diwash Bhattarai <diwashb999@gmail.com>
2025-07-28 22:20:54 +0545

    fix: ignore eslint error for no-console

--- src/hooks/use-local-storage.ts
+++ src/hooks/use-local-storage.ts
@@ -18,6 +18,7 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
       const item = window.localStorage.getItem(key);
       return item !== null ? (JSON.parse(item) as T) : initialValue;
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(`Error reading localStorage key "${key}":`, error);
       return initialValue;
     }
@@ -28,6 +29,7 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
     try {
       window.localStorage.setItem(key, JSON.stringify(storedValue));
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(`Error setting localStorage key "${key}":`, error);
     }
   }, [key, storedValue]);
